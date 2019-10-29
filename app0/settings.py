95a701ec5624908277f85907e9919786d1fd1d8b
Mark Seaman <Mark.Seaman@imac.net>
2019-10-29 08:39:37 -0600

    Create single page app lesson

--- app0/settings.py
+++ app0/settings.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 DEBUG=True
 SECRET_KEY = 'KDqYibgo1ZI4QIHOFInXmTy6wknXxWiii5DBal825FQgCXo5zA'
 MIDDLEWARE_CLASSES = ()
-ROOT_URLCONF = 'singlefile.settings'
+ROOT_URLCONF = 'settings'
 ALLOWED_HOSTS = ['*']
 
 # Home View
