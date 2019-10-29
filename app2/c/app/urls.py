9f63087f0ee98bebae27ae574cbc01c3adac19d4
Mark Seaman <Mark.Seaman@imac.net>
2019-10-29 15:20:41 -0600

    View inheritance

--- app2/c/app/urls.py
+++ app2/c/app/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import url
-from notes.views import home, notes, test
+from notes.views import home, missing, notes, test, unc
 
 
 
 # URL patterns to match
 urlpatterns = [
+    url('home', home),
     url('notes', notes),
     url('test', test),
-    url('', home),
+    url('unc',  unc),
+    url('', missing),
 ]
 
 
