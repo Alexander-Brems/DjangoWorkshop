3af4145fbc70e5ee9fa64b81f6818567281d2657
Mark Seaman <Mark.Seaman@imac.net>
2019-10-29 09:13:14 -0600

    app1 with a single file

--- app1/simple/urls.py
+++ app1/simple/urls.py
@@ -1,21 +1,31 @@
-"""simple URL Configuration
+from django.conf.urls import url
+from django.http import HttpResponse
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
 
+# HTML code to display
+html = '''<h1>App 1 - Simple App</h1>
+<p>
+    This is simple Django app that was created using the default command script.
+</p>
+<p>
+    This page will be shown for all requests to this server.
+</p>
+'''
+
+
+# Home View
+def home(request):
+    return HttpResponse(html)
+
+# Test page
+def test(request):
+    return HttpResponse("<h1>App 1 - Test page</h1><a href=x>home</a>")
+
+
+# URL patterns to match
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('test', test),
+    url('', home),
 ]
+
+
